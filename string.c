27d35a79034a5b983adb9b26af4b81ee1ee31456
HARSHVARDHAN CHAND <harshvardhan335@gmail.com>
2022-05-04 18:17:27 +0530

    second commit

--- string.c
+++ string.c
@@ -4,3 +4,12 @@
 //ASCII code take up 1 byte and Unicode code take up 2 bytes Hexadecimal representation is used  for storing Unicode code
 // printf("%d",'A'); will give you the ASCII code of 'A' i.e 65
 // char name[10] = "Hello";//automatically adds a null character at the end of the string char name[10] = {'H','e','l','l','o','\0'};
+int lenght(char *str)
+{
+    int i=0;
+    while(str[i]!='\0')
+    {
+        i++;
+    }
+    return i;
+}
